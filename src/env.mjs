a7d859c46a90cfd0b7e924c26970e3f64709f0e4
mofareed-97 <m.fareeed1997@gmail.com>
2023-07-15 23:43:17 +0300

    strip api

--- src/env.mjs
+++ src/env.mjs
@@ -4,7 +4,6 @@ import { z } from "zod";
 export const env = createEnv({
   server: {
     DATABASE_URL: z.string().url(),
-    OPEN_AI_API_KEY: z.string().min(1),
     STRIPE_API_KEY: z.string().min(1),
     STRIPE_WEBHOOK_SECRET: z.string(),
   },
